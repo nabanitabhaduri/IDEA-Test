3aaa912f8ec6f2c5d02022a47869fbdf4b169975
nabanita bhaduri <nabanita@idiosystech.com>
2023-05-04 13:11:56 +0530

    3nd commit

---
+++ src/sampletest/Hello.java
@@ -0,0 +1,8 @@
+package sampletest;
+
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("hello");
+    }
+}
--- src/sampletest/easybuy.java
+++ src/sampletest/easybuy.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+// test
 public class easybuy {
     public static void main(String[] args) {
         try {
